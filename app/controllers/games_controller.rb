78a3919db6d9c3228bbc722383abc0ae9d994d3e
Chris Adebiyi <oluvvafemi@gmail.com>
2022-07-28 16:04:43 +0200

    Create new instance for each game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,11 @@
 class GamesController < ApplicationController
+  before_action :setup_game
   def play; end
+
+  private
+
+  def setup_game
+    @game = current_user.games.create
+    redirect_to root_path, alert: 'Unable to launch new game' unless @game.save
+  end
 end
