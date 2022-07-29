ad4eefc9c3752ada72228e99e0b38b774525b557
Chris Adebiyi <oluvvafemi@gmail.com>
2022-07-29 16:22:30 +0200

    Clean up Users#show controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   def show
-    @wins = current_user.games.where('status = ?', 'win').count
-    @losses = current_user.games.where('status = ?', 'loss').count
-    @draws = current_user.games.where('status = ?', 'draw').count
-    @games_played = current_user.games.where.not(status: '').count
+    @user_data = user_data
 
     @leaders = User.joins(:games).where('games.status': 'win').select('name, count(games.status)').group(:name).order(count: :desc)
                    .limit(10).as_json
+  end
+
+  private
+
+  def user_data
+    {
+      wins: current_user.games.where('status = ?', 'win').count,
+      losses: current_user.games.where('status = ?', 'loss').count,
+      draws: current_user.games.where('status = ?', 'draw').count,
+      games_played: current_user.games.where.not(status: '').count
+    }
   end
 end
