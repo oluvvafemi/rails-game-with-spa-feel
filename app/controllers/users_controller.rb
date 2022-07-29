f420b420439f9bc92e2c3bac3634d14e566eaabd
Chris Adebiyi <oluvvafemi@gmail.com>
2022-07-29 13:05:45 +0200

    Move show queries to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,8 @@
 class UsersController < ApplicationController
-  def show; end
+  def show
+    @wins = current_user.games.where('status = ?', 'win').count
+    @losses = current_user.games.where('status = ?', 'loss').count
+    @draws = current_user.games.where('status = ?', 'draw').count
+    @games_played = current_user.games.where.not(status: '').count
+  end
 end
