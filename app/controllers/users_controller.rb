ba4b31605012e2d61b65918a987f24f4b4986541
Chris Adebiyi <oluvvafemi@gmail.com>
2022-07-29 15:25:55 +0200

    Implement leaderboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,8 @@ class UsersController < ApplicationController
     @losses = current_user.games.where('status = ?', 'loss').count
     @draws = current_user.games.where('status = ?', 'draw').count
     @games_played = current_user.games.where.not(status: '').count
+
+    @leaders = User.joins(:games).where('games.status': 'win').select('name, count(games.status)').group(:name).order(count: :desc)
+                   .limit(10).as_json
   end
 end
